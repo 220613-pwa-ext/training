7c3e94f1bfaf240d860cdae2c7b75568c1aee1db
Bach T <bach.tran@revature.com>
2022-06-14 16:11:44 -0400

    Add my-first-project-in-an-ide and add intro-to-variables

---
+++ week-1/intro-to-variables/variables.py
@@ -0,0 +1,29 @@
+# Comments, which are created using # symbol, allow for the interpreter to
+# ignore that text. It will not be considered part of the program
+# Comments are intended to make confusing code easier to understand for other
+# programmers
+# Comments should not be abused. You shouldn't overuse comments to explain
+# every single line of code.
+# Rule of thumb: Code should be easily self-explanatory as much as possible to
+# other developers
+
+# Variables allow you to store different values
+# For example, we create a variable with the name abc (abc is our identifier)
+# and we assign it a value of 100
+abc = 100
+print(abc)
+abc = "hello"  # str
+print(abc)
+
+x = 10  # int
+y = 20.0  # float
+print(x)
+print(y)
+
+print(abc)  # hello
+print("hello")  # hello
+
+# Datatypes
+print(type(abc))  # Should print out "<class 'str'>"
+print(type(x))  # Should print out "<class 'int'>"
+print(type(y))  # Should print out "<class 'float'>"
