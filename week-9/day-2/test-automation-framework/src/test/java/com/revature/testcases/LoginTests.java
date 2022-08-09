b74087929296909eade7d4db391a3f763deb89d9
Bach T <bach.tran@revature.com>
2022-08-09 17:53:13 -0400

    Add 3 negative test cases

--- week-9/day-2/test-automation-framework/src/test/java/com/revature/testcases/LoginTests.java
+++ week-9/day-2/test-automation-framework/src/test/java/com/revature/testcases/LoginTests.java
@@ -51,4 +51,94 @@ public class LoginTests {
         // So, you really should be using driver.quit(), not driver.close()
     }
 
+    @Test
+    public void validUsernameInvalidPassword() {
+        // Write some code to automate the test case
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://ec2-18-116-32-53.us-east-2.compute.amazonaws.com");
+
+        WebElement usernameInput = driver.findElement(By.id("username"));
+        WebElement passwordInput = driver.findElement(By.id("password"));
+
+        usernameInput.sendKeys("john_doe");
+        passwordInput.sendKeys("asdfsdf");
+
+        WebElement loginButton = driver.findElement(By.id("login-btn"));
+        loginButton.click();
+
+        WebDriverWait wdw = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wdw.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='error-message']/p")));
+
+        WebElement errorMessageParagraph = driver.findElement(By.xpath("//*[@id='error-message']/p"));
+        String actual = errorMessageParagraph.getText();
+        String expected = "Username and/or password is incorrect";
+
+        Assert.assertEquals(actual, expected);
+
+        driver.quit();
+    }
+
+    @Test
+    public void invalidUsernameInvalidPassword() {
+        // Write some code to automate the test case
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://ec2-18-116-32-53.us-east-2.compute.amazonaws.com");
+
+        WebElement usernameInput = driver.findElement(By.id("username"));
+        WebElement passwordInput = driver.findElement(By.id("password"));
+
+        usernameInput.sendKeys("fgdfljqwekr");
+        passwordInput.sendKeys("asdfsdf");
+
+        WebElement loginButton = driver.findElement(By.id("login-btn"));
+        loginButton.click();
+
+        WebDriverWait wdw = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wdw.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='error-message']/p")));
+
+        WebElement errorMessageParagraph = driver.findElement(By.xpath("//*[@id='error-message']/p"));
+        String actual = errorMessageParagraph.getText();
+        String expected = "Username and/or password is incorrect";
+
+        Assert.assertEquals(actual, expected);
+
+        driver.quit();
+    }
+
+    @Test
+    public void invalidUsernameValidPassword() {
+        // Write some code to automate the test case
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://ec2-18-116-32-53.us-east-2.compute.amazonaws.com");
+
+        WebElement usernameInput = driver.findElement(By.id("username"));
+        WebElement passwordInput = driver.findElement(By.id("password"));
+
+        usernameInput.sendKeys("fgdfljqwekr");
+        passwordInput.sendKeys("abc12345");
+
+        WebElement loginButton = driver.findElement(By.id("login-btn"));
+        loginButton.click();
+
+        WebDriverWait wdw = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wdw.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='error-message']/p")));
+
+        WebElement errorMessageParagraph = driver.findElement(By.xpath("//*[@id='error-message']/p"));
+        String actual = errorMessageParagraph.getText();
+        String expected = "Username and/or password is incorrect";
+
+        Assert.assertEquals(actual, expected);
+
+        driver.quit();
+    }
+
 }
